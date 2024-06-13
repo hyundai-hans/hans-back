9d0b50f76fc4eadd0f009d982013ca8536d68447
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-13 18:32:47 +0900

    add: add the authentication feature

--- src/main/java/com/handsome/mall/auth/config/SecurityConfig.java
+++ src/main/java/com/handsome/mall/auth/config/SecurityConfig.java
@@ -1,12 +1,43 @@
 package com.handsome.mall.auth.config;
 
+import com.handsome.mall.auth.handler.TokenHandler;
+import com.handsome.mall.auth.handler.UserAuthenticationManager;
+import com.handsome.mall.member.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+  private final UserRepository userRepository;
+
+
+  @Bean
+  public UsernamePasswordAuthenticationFilter storeManagerAuthenticationFilter() {
+    StoreManagerAuthenticationFilter authenticationFilter = new StoreManagerAuthenticationFilter(
+        authenticationSuccessHandler, storeAuthenticationManager());
+    authenticationFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler);
+    authenticationFilter.setFilterProcessesUrl("/**/stores/login");
+    return authenticationFilter;
+  }
+
+
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+
+  @Bean
+  public AuthenticationManager userAuthenticationManager() {
+    return new UserAuthenticationManager(userRepository);
+  }
 }
