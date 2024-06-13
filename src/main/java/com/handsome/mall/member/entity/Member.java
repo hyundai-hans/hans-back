3bf8e8dab3ade05e9ddda5d8c0098e4d854534b6
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-13 16:52:28 +0900

    add: add the member entity

---
+++ src/main/java/com/handsome/mall/member/entity/Member.java
@@ -0,0 +1,12 @@
+package com.handsome.mall.member.entity;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long id;
+    private String nickname;
+    private String password;
+    private String profileImg;
+    private String role;
+}
