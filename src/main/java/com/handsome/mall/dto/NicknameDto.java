be68fde1711096aa9613b86e9a476a9e1720cbda
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-17 12:22:18 +0900

    feat: add the two of duplication check api

---
+++ src/main/java/com/handsome/mall/dto/NicknameDto.java
@@ -0,0 +1,15 @@
+package com.handsome.mall.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class NicknameDto {
+
+  private String nickname;
+}
